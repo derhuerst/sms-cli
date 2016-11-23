96db851417a33718a129574c7ea26045437b54ec
Jannis R <mail@jannisr.de>
2016-11-23 11:10:11 +0100

    mockup

--- bin.js
+++ bin.js
@@ -2,12 +2,13 @@
 'use strict'
 
 const yargs = require('yargs')
-// const chalk = require('chalk')
+const chalk = require('chalk')
+const Table = require('cli-table2')
+const ms = require('ms')
 // const figures = require('figures')
-// const ms = require('ms')
 // const so = require('so')
 
-const client = require('.')
+const sms = require('.')
 
 
 
@@ -23,4 +24,46 @@ Usage:
 
 
 
-// todo
+const showError = (msg) => {
+	process.stdout.write(chalk.red(msg) + '\n')
+}
+
+const table = () => new Table({
+	chars: {
+		top:    '', 'top-mid':    '', 'top-left':    '', 'top-right':    '',
+		bottom: '', 'bottom-mid': '', 'bottom-left': '', 'bottom-right': '',
+		left:   '', 'left-mid':   '',  mid:          '', 'mid-mid':      '',
+		right:  '', 'right-mid':  '',  middle:       ' '
+	},
+	style: {'padding-left': 1, 'padding-right': 0}
+})
+
+const renderMessages = (messages) => {
+	const out = table()
+	for (let message of messages) {
+		out.push([
+			ms(Date.now() - message.when),
+			message.to,
+			message.text.slice(0, 30)
+		])
+	}
+	return out.toString() + '\n'
+}
+
+
+
+if (!process.env.TWILIO_SID) {
+	showError('Missing TWILIO_SID env variable.')
+	process.exit(1)
+}
+if (!process.env.TWILIO_TOKEN) {
+	showError('Missing TWILIO_TOKEN env variable.')
+	process.exit(1)
+}
+const client = sms(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+
+client.list()
+.then((messages) => {
+	// console.log(messages)
+	process.stdout.write(renderMessages(messages))
+}, console.error)
--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict'
 
-// todo
+const twilio = require('twilio')
+
+const sms = (sid, token) => {
+	const client = twilio(sid, token)
+
+	// mock
+	return {
+		list: () => Promise.resolve([
+			{
+				from: '+49494949494',
+				to: '+21212121212',
+				text: 'foo bar baz',
+				when: Date.now() - 30 * 1000
+			},
+			{
+				from: '+49494949494',
+				to: '+60606060606',
+				text: 'Hello World!',
+				when: Date.now() - 3.5 * 60 * 1000
+			}
+		])
+	}
+
+	return {
+		list: () => client.sms.messages.list()
+	}
+}
+
+module.exports = sms
