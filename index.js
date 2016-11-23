9c8598b1a7950f0059af4d9bb003525ff26ead21
Jannis R <mail@jannisr.de>
2016-11-24 00:43:46 +0100

    implement Twilio client

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ const twilio = require('twilio')
 
 const sms = (sid, token) => {
 	const client = twilio(sid, token)
-
-	// mock
 	return {
-		list: () => Promise.resolve([
-			{
-				from: '+49494949494',
-				to: '+21212121212',
-				text: 'foo bar baz',
-				when: Date.now() - 30 * 1000
-			},
-			{
-				from: '+49494949494',
-				to: '+60606060606',
-				text: 'Hello World!',
-				when: Date.now() - 3.5 * 60 * 1000
-			}
-		])
-	}
+		  numbers: () =>
+			client.incomingPhoneNumbers.list().then((data) =>
+				data.incomingPhoneNumbers.map((number) => ({
+					id: number.sid,
+					name: number.friendlyName,
+					nr: number.phoneNumber,
+				}))
+			)
 
-	return {
-		list: () => client.sms.messages.list()
+		, send: (from, to, text) =>
+			client.sms.messages.post({from, to, body: text})
+
+		, list: () =>
+			client.sms.messages.list().then((data) =>
+				data.smsMessages.map((message) => ({
+					id: message.sid,
+					when: message.dateSent,
+					from: message.from,
+					to: message.to,
+					text: message.body,
+					outbound: message.direction.slice(0, 7) === 'inbound'
+				}))
+			)
 	}
 }
 
